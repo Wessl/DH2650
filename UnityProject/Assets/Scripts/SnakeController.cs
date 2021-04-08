f9aec024fe77ab13e17558c6b61075c4f4f2be5d
Love Wessman <lwessman@kth.se>
2021-04-08 22:10:34 +0200

    Snake attack added, fixed some snake physics
    Snake will now attack automatically when player is near the front of the snake.
    
    Attacking involves stopping slowly, making the attack. Currently no damage. Will keep moving after the attack if player is no longer in front.
    
    Snake physics are a bit better, I just fiddled with the rigidbody settings a bit.

--- UnityProject/Assets/Scripts/SnakeController.cs
+++ UnityProject/Assets/Scripts/SnakeController.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using Vector2 = UnityEngine.Vector2;
 
 /* TODO for snake:
  * Add player detection
@@ -14,7 +16,9 @@ public class SnakeController : MonoBehaviour
     private Animator snakeAnimator;
     private Rigidbody2D rb;
     public float moveSpeed;
-   
+    [Tooltip("Time it takes to accelerate/decelerate between min and max movespeed")]
+    public float slowDownTime;      
+
     void Start()
     {
         snakeAnimator = GetComponent<Animator>();
@@ -28,21 +32,36 @@ public class SnakeController : MonoBehaviour
 
     void Move()
     {
-        rb.velocity = transform.right * moveSpeed * Time.deltaTime;
-        if (rb.velocity.magnitude > 0)
+        if (!snakeAnimator.GetCurrentAnimatorStateInfo(0).IsName("snake_attackanim"))
         {
-            snakeAnimator.SetBool("isMoving", true);
+            // If snake is not currently stuck in "attack" animation,
+            rb.velocity = new Vector2(transform.right.x * moveSpeed * Time.deltaTime, rb.velocity.y);
+            Debug.Log(transform.right);
+            if (rb.velocity.magnitude > 0)
+            {
+                snakeAnimator.SetBool("isMoving", true);
+            }
+        }
+        else
+        {
+            // If snake is stuck in attack animation, Lerp movespeed to 0. 
+            rb.velocity = Vector2.Lerp(rb.velocity, Vector2.zero, slowDownTime * Time.deltaTime);
         }
     }
     
-    void OnTriggerEnter2D(Collider2D col)
+    void OnTriggerStay2D(Collider2D col)
     {
         // Temporary solution? Future: Make unique tag for walls? Or other solution... 
         if (col.CompareTag("Ground"))
         {
+            // Rotate instantly:
             transform.Rotate(0, 180f, 0);
+
+        }
+        // If this detects a player, it will trigger an Attack animation. (No damage at the moment)
+        else if (col.CompareTag("Player"))
+        {
+            snakeAnimator.SetTrigger("attack");
         }
     }
-    
-    
 }
