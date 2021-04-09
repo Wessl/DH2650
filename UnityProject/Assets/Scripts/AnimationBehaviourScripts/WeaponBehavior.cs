42b07877c8e063f78a6b8a2790954c97dd9c2873
Love Wessman <lwessman@kth.se>
2021-04-09 15:49:08 +0200

    Make sword swingable :D (Right click)

---
+++ UnityProject/Assets/Scripts/AnimationBehaviourScripts/WeaponBehavior.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBehavior : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // If you don't reset the trigger On Exit, you can activate another sword swing while it's already swinging, 
+        // causing another swing after the animation finishes without the player clicking then. 
+        animator.ResetTrigger("attack");
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
--- UnityProject/Assets/Scripts/Weapon.cs
+++ UnityProject/Assets/Scripts/Weapon.cs
@@ -1,22 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Timeline;
 
 // Class to be used for Weapons. Weapon type, how it's holstered, actions when attacking etc. should be done from this. 
 public class Weapon : MonoBehaviour
 {
+    private Animator anim;
     private PlayerMovement pm;
+    private PolygonCollider2D col;
     private SpriteRenderer weaponSprite;
+    
 
     void Start()
     {
+        anim = GetComponent<Animator>();
         pm = GetComponentInParent<PlayerMovement>();
+        col = GetComponent<PolygonCollider2D>();
         weaponSprite = GetComponent<SpriteRenderer>();
+
+        col.enabled = false;    // Weapon collider should be disabled while holstered
     }
     
     void Update()
     {
         HolsterOrientation();
+        Attack();
     }
 
     // This is kind of dirty code, but it works. Makes sure the sword is resting in it's holster on the correct side of the frog depending on which way frog is facing. 
@@ -29,6 +38,20 @@ public class Weapon : MonoBehaviour
         else
         {
             weaponSprite.sortingOrder = 0;
+        }
+    }
+
+    void Attack()
+    {
+        // Right click to attack
+        if (Input.GetMouseButtonDown(1))
+        {
+            // Remove from holster
+            // Activate weapon collider
+            col.enabled = true;
+            // Play animation
+            anim.SetTrigger("attack");
+            // Put back into holster
         }
     }
 }
