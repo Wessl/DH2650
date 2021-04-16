f0beb661ce2a00b9146e00a1ffedec68672a5835
Wessl <lovewessman@gmail.com>
2021-04-16 15:16:25 +0200

    Wasp animation states functional. Fix #10.
    closes #10 (good for performance)

--- UnityProject/Assets/Scripts/AnimationBehaviourScripts/GenericEnemyAnimationBehaviour.cs
+++ UnityProject/Assets/Scripts/AnimationBehaviourScripts/GenericEnemyAnimationBehaviour.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnakeAnimationBehaviour : StateMachineBehaviour
+public class GenericEnemyAnimationBehaviour : StateMachineBehaviour
 {
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
