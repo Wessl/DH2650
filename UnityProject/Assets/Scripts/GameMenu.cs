537037a074c20406d9cf9f61a78d0ccaf100ed7b
bratfors <rbratfors@gmail.com>
2021-05-25 20:59:39 +0200

    canvas prefab and bug fixes
    I forgot to update the game canvas prefab.
    
    The boss camera trigger in the cave didn't have it's cameraCV variable assigned, I don't know if I mucked it up somehow, but it seems to work now.
    
    The scene loading needed initial spawn position, which is added now.

--- UnityProject/Assets/Scripts/GameMenu.cs
+++ UnityProject/Assets/Scripts/GameMenu.cs
@@ -125,17 +125,23 @@ public class GameMenu : MonoBehaviour
         int i = button;
         switch (button)
         {
+            case 0:
+                SceneHandler.Instance.checkpointPosition = new Vector3(-20, 4.892499f, 0);
+                break;
             case 1:
                 SceneHandler.Instance.checkpointPosition = new Vector3(387, 344, 0);
                 i = 0;
                 break;
             case 2:
+                SceneHandler.Instance.checkpointPosition = new Vector3(300, 116, 0);
                 i = 1;
                 break;
             case 3:
+                SceneHandler.Instance.checkpointPosition = new Vector3(170, 47, 0);
                 i = 2;
                 break;
             case 4:
+                SceneHandler.Instance.checkpointPosition = new Vector3(-16, 14, 0);
                 i = 3;
                 break;
             default:
